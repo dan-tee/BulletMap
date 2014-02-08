3962068184b29cd2d321ce043d1745f964232e90
aselims <selim.2k@gmail.com>
2014-02-08 17:10:32 +0100

    API ready

---
+++ BulletMapAPInode/myApp.js
@@ -0,0 +1,24 @@
+/**
+ * Created by aselims on 1/14/14.
+ #hack4Good Selim
+ */
+ 
+var express = require('express'),
+
+    http = require('http'),
+    bulletsdb = require('./routes/bulletsdb');
+
+var app = express();
+
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.use(express.bodyParser());
+});
+
+app.get('/bullets', bulletsdb.findAll);
+app.post('/bullet', bulletsdb.addOnebullet);
+app.get('/bullet/:id', bulletsdb.findOnebullet);
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
+});
