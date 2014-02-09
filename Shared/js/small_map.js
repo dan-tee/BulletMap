7b68f897ef9e5775163c38ca0449d6da1c731523
Dan-Tee <github@dan-t.de>
2014-02-09 11:53:11 +0100

    select location on map

--- Shared/js/small_map.js
+++ Shared/js/small_map.js
@@ -4,7 +4,28 @@ function createSmallMap(){
     }
 
     small_map = createMap('small-map');
+    small_map.on('click', onMapClick);
 }
 
+function onMapClick(e){
+    var latlng = e.latlng;
+    writeLocationToUi(latlng);
+    setMarker(latlng);
+}
+
+function writeLocationToUi(latlng){
+    $("#latitude").val(latlng.lat);
+    $("#longitude").val(latlng.lng);
+}
+
+function setMarker(latlng){
+    if (!marker){
+        marker = L.marker(latlng, {draggable: 'true'}).addTo(small_map);
+    }
+
+    marker.setLatLng(latlng);
+}
+
+var marker;
 var small_map;
 $(document).on('pageshow', '#bullet-search', createSmallMap);
