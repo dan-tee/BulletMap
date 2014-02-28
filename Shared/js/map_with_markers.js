867ddde3aefccb19fceda8497f94b964e00020d0
Dan-Tee <github@dan-t.de>
2014-02-28 19:15:06 +0100

    refresh markers on each visit

--- Shared/js/map_with_markers.js
+++ Shared/js/map_with_markers.js
@@ -1,45 +1,57 @@
-function requestDataAndMark(){
-    var url = server+"/found_shells";
-    $.getJSON(url, markFoundBullets);
-}
-
-function markFoundBullets(findingsByCountries){
-    // evenly distribute countries on the color circle
-    var hueFraction = 360 / findingsByCountries.length;
-
-    for(var i = 0; i < findingsByCountries.length; i++){
-        var colorString = "hsv(" + hueFraction * i + ", 50%, 50%)";
-        var color = tinycolor(colorString).toHexString();
-        addLegendItem(color, findingsByCountries[i].Origin)
-        paintForCountry(color, findingsByCountries[i]);
-    }
-}
-
-function addLegendItem(color, origin){
-    $("#legend ul").append('<li><font color="' + color + '">' + origin + "</font></li>")
-}
-
-function paintForCountry(color, findings){
-    findings.Locations.forEach(function(location){
-        L.circleMarker([location.Latitude, location.Longitude], {
-            color: color,
-            radius: 8,
-            fillOpacity: 0.5
-        }).addTo(map);
-    });
-}
-
-function createAndMarkMap(){
-    if (window.map && window.map instanceof L.Map){
-        return;
+(function(){
+    function requestDataAndMark(){
+        var url = server+"/found_shells";
+        $.getJSON(url, markFoundBullets);
     }
 
-    map = createMap('map');
-    requestDataAndMark();
-}
+    function removeOldMarks(){
+        $("#legend").find("ul").empty();
+        markerGroup.clearLayers();
+    }
 
-var map;
-$(document).on('pageshow', '#bullet-map', createAndMarkMap);
+    function markFoundBullets(findingsByCountries){
+        removeOldMarks();
+
+        // evenly distribute countries on the color circle
+        var hueFraction = 360 / findingsByCountries.length;
+
+        for(var i = 0; i < findingsByCountries.length; i++){
+            var colorString = "hsv(" + hueFraction * i + ", 50%, 50%)";
+            var color = tinycolor(colorString).toHexString();
+            addLegendItem(color, findingsByCountries[i].Origin)
+            paintForCountry(color, findingsByCountries[i]);
+        }
+
+        markerGroup.addTo(map);
+    }
+
+    function addLegendItem(color, origin){
+        $("#legend").find("ul").append('<li><font color="' + color + '">' + origin + "</font></li>")
+    }
+
+    function paintForCountry(color, findings){
+        findings.Locations.forEach(function(location){
+            var marker = L.circleMarker([location.Latitude, location.Longitude], {
+                color: color,
+                radius: 8,
+                fillOpacity: 0.5
+            });
+            markerGroup.addLayer(marker);
+        });
+    }
+
+    function createAndMarkMap(){
+        if (!map || !map instanceof L.Map){
+            map = createMap('map');
+        }
+
+        requestDataAndMark();
+    }
+
+    var map;
+    var markerGroup = L.layerGroup();
+    $(document).on('pageshow', '#bullet-map', createAndMarkMap);
+}());
 
 
 
