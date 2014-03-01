fd6a1f7026d18405748a2c5539be07d8b0d151de
Dan-Tee <github@dan-t.de>
2014-03-01 11:17:26 +0100

    change to MapQuest

--- Shared/js/map_util.js
+++ Shared/js/map_util.js
@@ -1,9 +1,9 @@
 function createMap(divId){
     var map = L.map(divId);
-    var osmUrl='http://{s}.tile.osm.org/{z}/{x}/{y}.png';
-    var osmAttrib='Map data © OpenStreetMap contributors';
-    var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
+    //var osmUrl='http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+    //var osmAttrib='Map data © OpenStreetMap contributors';
+    //var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
     map.setView(new L.LatLng(51.3, 0.7),2);
-    map.addLayer(osm);
+    map.addLayer(MQ.mapLayer());
     return map;
 }
